62e972db532bedffca62fb865286db455901eeb2
li wang <heiyai123@hotmail.com>
2020-11-11 19:07:29 -0800

    clean baseservice

--- Employee.Service/BaseService.cs
+++ Employee.Service/BaseService.cs
@@ -2,11 +2,8 @@
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Employee.Service
 {
