299ea79bbfd7a1d711c8d0166e692e7bd42cc878
li wang <heiyai123@hotmail.com>
2020-11-12 22:08:42 -0800

    Task delete done

--- Employee.Model/Models/Employee.cs
+++ Employee.Model/Models/Employee.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -8,11 +10,22 @@ namespace Employee.Model
     public partial class Employee
     {
         public int EmployeeId { get; set; }
+        [Description("名")]
+        [Required]
+        [MaxLength(50)]
         public string FirstName { get; set; }
+        [Description("姓")]
+        [Required]
+        [MaxLength(50)]
         public string LastName { get; set; }
+        [Required]
+        [Description("录取日期")]
+        [DataType(DataType.DateTime)]
         public DateTime HiredDate { get; set; }
 
+        [Description("名")]
         public string FName { get { return FirstName.Length > 5 ? FirstName.Substring(0, 5) + "..." : FirstName; } }
+        [Description("姓")]
         public string LName { get { return LastName.Length > 5 ? LastName.Substring(0, 5) + "..." : LastName; } }
     }
 }
--- Employee.Model/Models/Task.cs
+++ Employee.Model/Models/Task.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -9,12 +11,21 @@ namespace Employee.Model
     {
         public int TaskId { get; set; }
         public int EmployeeId { get; set; }
+        [MaxLength(50)]
+        [Required]
+        [Description("任务名称")]
         public string TaskName { get; set; }
+        [Description("开始时间")]
+        [Required]
+        [DataType(DataType.DateTime)]
         public DateTime StartTime { get; set; }
+        [Description("结束时间")]
+        [Required]
+        [DataType(DataType.DateTime)]
         public DateTime Deadline { get; set; }
 
         public virtual Employee Employee { get; set; }
-
+        [Description("任务名称")]
         public string TName { get { return TaskName.Length > 15 ? TaskName.Substring(0, 15) + "..." : TaskName; } }
         
     }
