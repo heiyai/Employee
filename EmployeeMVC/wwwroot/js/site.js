46fdd40eb704dd81cf63e16313c30c83aef56222
li wang <heiyai123@hotmail.com>
2020-11-12 16:35:04 -0800

    add check

--- EmployeeMVC/wwwroot/js/site.js
+++ EmployeeMVC/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function AddEpyValid() {
+    var fName = $("#FirstName").val()
+    var lName = $("#LastName").val()
+    if (fName == "" || lName == "") {
+        alert("请填写姓与名！")
+        return false;
+    }
+    return true;
+}
